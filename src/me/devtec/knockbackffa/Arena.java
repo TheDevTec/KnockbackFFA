6733e7880fbfc89da8892da8fc0eefb4b42380a1
StraikerinaCZ <Straiker123@email.cz>
2021-06-16 21:34:09 +0200

    Update

--- src/me/devtec/knockbackffa/Arena.java
+++ src/me/devtec/knockbackffa/Arena.java
@@ -2,41 +2,50 @@ package me.devtec.knockbackffa;
 
 import java.util.Arrays;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+
+import me.devtec.theapi.TheAPI;
+import me.devtec.theapi.apis.ItemCreatorAPI;
+import me.devtec.theapi.utils.datakeeper.Data;
 
 public class Arena {
 	public Location spawn;
 	public ItemStack[] itemStacks =
             Arrays.asList(
-                    new ItemStack(Material.STICK, 1),
-                    new ItemStack(Material.BOW, 1),
-                    new ItemStack(Material.ARROW, 1),
-                    new ItemStack(Material.ENDER_PEARL, 1),
-                    new ItemStack(Material.STONE_PRESSURE_PLATE, 1),
-                    new ItemStack(Material.RED_TERRACOTTA, 64)
-            ).toArray(new ItemStack[0]);
+                    addEnchants(ItemCreatorAPI.create(Material.STICK, 1, "&cKnockback stick")),
+                    ItemCreatorAPI.create(Material.BOW, 1, "&3Luk"),
+                    ItemCreatorAPI.create(Material.ARROW, 1, "&7Šíp"),
+                    ItemCreatorAPI.create(Material.ENDER_PEARL, 1, "&5Ender Pearl"),
+                    ItemCreatorAPI.create(Material.LIGHT_WEIGHTED_PRESSURE_PLATE, 1, "&eJumpPad"),
+                    ItemCreatorAPI.create(Material.WHITE_TERRACOTTA, 64, "&cBlocks")).toArray(new ItemStack[0]);
 
-    public void join(final Player target) {
+    public Arena(Data data) {
+    	spawn=data.getAs("spawn", Location.class);
+	}
+
+	private ItemStack addEnchants(ItemStack create) {
+		create.addUnsafeEnchantment(Enchantment.KNOCKBACK, 5);
+		return create;
+	}
+
+	public void join(Player target) {
     	target.teleport(spawn);
+    	target.getInventory().clear();
+    	for(ItemStack st : itemStacks)
+    		target.getInventory().addItem(st.clone());
     }
 
-    public void moveAll(final Arena arena) {
-
+    public void moveAll(Arena arena) {
         if (arena != null)
-            for (final Player player : Bukkit.getOnlinePlayers())
+            for (Player player : TheAPI.getOnlinePlayers())
                 arena.join(player);
-
     }
 
     public ItemStack[] getItemStacks() {
         return itemStacks;
-    }
-
-    public static Arena getArena() {
-        return new Arena();
     }
 }
