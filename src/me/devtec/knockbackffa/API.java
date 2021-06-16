b61be672a125fe7f4c42938320fa363cedafec23
petulikan1 <petulikan@gmail.com>
2021-06-16 21:50:52 +0200

    Repair API

--- src/me/devtec/knockbackffa/API.java
+++ src/me/devtec/knockbackffa/API.java
@@ -6,14 +6,14 @@ import java.util.Map;
 import me.devtec.theapi.TheAPI;
 
 public class API {
-	protected static Map<String, Arena> arenas = new HashMap<>();
-	protected static Arena arena;
-	
-	public static Arena nextArena() {
-		return arena=TheAPI.getRandomFromCollection(arenas.values());
-	}
-	
-	public static Map<String, Arena> getArenas(){
-		return arenas;
-	}
+    protected static Map<String, Arena> arenas = new HashMap<>();
+    protected static Arena arena;
+
+    public static Arena nextArena() {
+        return arena=TheAPI.getRandomFromCollection(arenas.values());
+    }
+
+    public static Map<String, Arena> getArenas(){
+        return arenas;
+    }
 }
