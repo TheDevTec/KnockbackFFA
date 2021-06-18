a643fa7101f0e9e582bc70e065cbee99c731e63d
petulikan1 <petulikan@gmail.com>
2021-06-18 02:40:12 +0200

    .

--- src/me/devtec/knockbackffa/KBFFACmd.java
+++ src/me/devtec/knockbackffa/KBFFACmd.java
@@ -2,12 +2,16 @@ package me.devtec.knockbackffa;
 
 import java.io.File;
 
+import me.devtec.theapi.utils.StringUtils;
+import me.devtec.theapi.utils.datakeeper.DataType;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 import me.devtec.theapi.TheAPI;
 import me.devtec.theapi.utils.datakeeper.Data;
+import org.bukkit.entity.Player;
 
 public class KBFFACmd implements CommandExecutor {
 
@@ -51,13 +55,40 @@ public class KBFFACmd implements CommandExecutor {
 			return true;
 		}
 		if(args[0].equalsIgnoreCase("setspawn")){
-
+			if(!(s instanceof Player))return true;
+			if(args.length==1){
+				TheAPI.msg("&e/KBFFA SetSpawn <name>", s);
+				return true;
+			}
+			if(!API.arenas.containsKey(args[1])){
+				TheAPI.msg("&eArena "+args[1]+" not exist", s);return true;}
+			Location l = ((Player)s).getLocation();
+			API.arenas.get(args[1]).spawn=l;
+			Data d = new Data("plugins/KnockbackFFA/Arenas/"+args[1]+".yml");
+			d.set("Spawn",l);
+			d.save(DataType.YAML);
+			TheAPI.msg("&eSpawn set for arena &b"+args[1]+" &eat &9"+ StringUtils.fixedFormatDouble(l.getX())+" "+StringUtils.fixedFormatDouble(l.getY())+" "+StringUtils.fixedFormatDouble(l.getZ()), s);
+			return true;
 		}
 		if(args[0].equalsIgnoreCase("teleport")){
-
+			if(!(s instanceof Player))return true;
+			if(args.length==1){
+				TheAPI.msg("&e/KBFFA Teleport <name>", s);
+				return true;
+			}
+			if(!API.arenas.containsKey(args[1])){
+				TheAPI.msg("&eArena "+args[1]+" not exist", s);return true;}
+			((Player)s).teleport(API.arenas.get(args[1]).spawn);
+			TheAPI.msg("&eTeleported to arena &b"+args[1],s);
+			return true;
 		}
 		if(args[0].equalsIgnoreCase("nextarena")){
-
+			if(args.length==1){
+				TheAPI.msg("&e/KBFFA Nextarena", s);
+				return true;
+			}
+			API.arena.moveAll(API.nextArena());
+			return true;
 		}
 		return true;
 	}
